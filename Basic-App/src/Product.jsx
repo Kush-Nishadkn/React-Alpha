fc825f7002fddc716088b7d756db8ab785f03905
Delta StudentKush <nkush995@gmail.com>
2024-04-02 19:40:01 +0530

    first commit

---
+++ Basic-App/src/Product.jsx
@@ -0,0 +1,15 @@
+import "./Product.css";
+
+function Product(){
+    
+    return(
+        <>
+             <h3>Product Title</h3>
+            <h5>Product Description </h5>
+        </>
+       
+        
+    
+    ); 
+}
+export default Product;
---
+++ Basic-App/src/ProductTab.jsx
@@ -0,0 +1,14 @@
+import Product from "./Product.jsx";
+
+function ProductTab(){
+    let features=["hi-tech","durable","fast"];
+    return(
+        <>
+          <Product/> 
+          <Product/> 
+          <Product/>  
+        </>
+    )
+}
+
+export default ProductTab;
